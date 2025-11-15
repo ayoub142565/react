550ed8288c8dbb96f227ebc870aeafaabc4fe271
ayoub kharraz <2005012000144@ofppt-edu.ma>
2025-11-15 11:31:50 +0100

    code 1 added

---
+++ code.js
@@ -0,0 +1,12 @@
+class etudiant {
+    constructor(nom, age) {
+        this.nom = nom
+        this.age = age
+    }
+}
+
+let e1 = new etudiant("ayoub", 22)
+let e2 = new etudiant("walid", 20)
+
+console.log(e1)
+console.log(e2)
