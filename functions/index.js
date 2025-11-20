8fc1a8962e59c5035f158340da14338ab01d84d7
ayoub142565 <2005012000144@ofppt-edu.ma>
2025-11-20 11:15:04 +0100

    reduce added

--- functions/index.js
+++ functions/index.js
@@ -11,3 +11,16 @@ let nums = [2,5,8,7,3];
 newnums = nums.map(function(num){return num * 3;});
 
 console.log(newnums);
+
+//reduce
+const clients = [{nom:"rami", montant:4500},
+                    {nom:"karimi", montant:2300},
+                    {nom:"chaouki", montant:5500},
+                    {nom:"ramooun", montant:7700}];
+
+const totalMontant = clients.reduce(function(total, client){
+    console.log(total, client.montant);
+    return (total < client.montant ? client.montant : total);
+}, 0);
+
+console.log(totalMontant);
